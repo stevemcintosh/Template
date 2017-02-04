2a55e629821b94c2f528f1e4924fa208d6ecc8a3
Stephen J McIntosh <info@sovative.com>
2017-02-04 12:32:30 +1100

    Removed Fabric and Crashlytics from AppDelegate

--- Source_iOS/User Interface/Controllers/AppController.swift
+++ Source_iOS/User Interface/Controllers/AppController.swift
@@ -1,7 +1,7 @@
 import UIKit
 
-import Fabric
-import Crashlytics
+//import Fabric
+//import Crashlytics
 
 import ProcedureKit
 import ProcedureKitMobile
@@ -13,7 +13,7 @@ class AppController: UISplitViewController {
     /// Shared instance
 	static let shared = AppController(nibName: nil, bundle: nil)
 
-	lazy var fabric: Fabric = Fabric.with([Crashlytics.self])
+//	lazy var fabric: Fabric = Fabric.with([Crashlytics.self])
 	
 	let procedureQueue = ProcedureQueue()
 
@@ -81,7 +81,7 @@ class AppController: UISplitViewController {
 	}
 	
 	open func configureBaseApplication() -> Void {
-		Answers.logCustomEvent(withName: "configureBaseApplication", customAttributes: ["Category" : "App Launched", "configureBaseApplication" : 1])
+//		Answers.logCustomEvent(withName: "configureBaseApplication", customAttributes: ["Category" : "App Launched", "configureBaseApplication" : 1])
 
 		UIApplication.shared.setMinimumBackgroundFetchInterval(UIApplicationBackgroundFetchIntervalMinimum)
 		
@@ -115,29 +115,29 @@ class AppController: UISplitViewController {
 	}
 	
 	fileprivate func configureApplicationMonitoringTools() -> Swift.Void {
-		fabric.debug = true
+//		fabric.debug = true
 	}
 	
-	let log = SwiftyBeaver.self
+//	let log = SwiftyBeaver.self
 	fileprivate func configureLogging() -> Swift.Void {
-		let console = ConsoleDestination()
-		log.addDestination(console)
-		
-		ProcedureKit.LogManager.logger = { message, severity, file, function, line in
-			switch severity {
-			case .verbose:
-				self.log.verbose(message, file, function, line: line)
-			case .notice:
-				self.log.debug(message, file, function, line: line)
-			case .info:
-				self.log.info(message, file, function, line: line)
-			case .warning:
-				self.log.warning(message, file, function, line: line)
-			case .fatal:
-				self.log.error(message, file, function, line: line)
-			}
-		}
-		ProcedureKit.LogManager.severity = .info
+//		let console = ConsoleDestination()
+//		log.addDestination(console)
+//		
+//		ProcedureKit.LogManager.logger = { message, severity, file, function, line in
+//			switch severity {
+//			case .verbose:
+//				self.log.verbose(message, file, function, line: line)
+//			case .notice:
+//				self.log.debug(message, file, function, line: line)
+//			case .info:
+//				self.log.info(message, file, function, line: line)
+//			case .warning:
+//				self.log.warning(message, file, function, line: line)
+//			case .fatal:
+//				self.log.error(message, file, function, line: line)
+//			}
+//		}
+//		ProcedureKit.LogManager.severity = .info
 	}	
 }
 
