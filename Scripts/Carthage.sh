080964d6b7181e22605831b1aeb74ba5293d109e
stephenmcintosh <stephenmcintosh@steves-macbook-air.local>
2017-02-26 15:59:24 +1100

    create External directory if it doesn't exist -- needs for Xcode server

--- Scripts/Carthage.sh
+++ Scripts/Carthage.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 echo pwd
+if [ ! -d "$SRCROOT/../External/" ]; then
+	mkdir "$SRCROOT/../External/"
+fi
+
 if [ ! -d "$SRCROOT/../External/Carthage/" ]; then
 	cd ../External/ && carthage bootstrap --platform iOS
 fi
